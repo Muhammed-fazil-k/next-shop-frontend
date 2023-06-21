e9e774eeeca61703261b9133ddf464be73cafeff
fazil <muhammedfazil104@gmail.com>
2023-06-21 14:29:14 +0530

    Tsx support

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Home:React.FC = () => {
+  return (
+    <>
+     <Head>
+      <title>Next shop</title>
+     </Head>
+     <main>
+      <h1>Next shop</h1>
+     </main>
+    </>
+  )
+  
+}
+export default Home;
