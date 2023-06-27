078182e080e0437727e5fb32555022cce60429e3
fazil <muhammedfazil104@gmail.com>
2023-06-27 21:34:37 +0530

    Login Api route

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,12 +1,35 @@
-import {NextApiHandler} from 'next'
-const handleLogin:NextApiHandler = (req, res) => {
+import { NextApiHandler } from "next";
+import { fetchJson } from "../../lib/api";
+const handleLogin: NextApiHandler = async (req, res) => {
   if (req.method !== "POST") {
     //405 it doesnt support other methods
     res.status(405).end();
-    return
+    return;
   }
-  console.log(req.body);
-  res.status(200).json({});
-}
+  const { email, password } = req.body;
+
+  try {
+    const { jwt, user } = await fetchJson("http://localhost:1337/auth/local", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        identifier: email,
+        password,
+      }),
+    });
+    res.status(200).json({
+      id: user.id,
+      username: user.username,
+    });
+  } catch (err) {
+    //invalid credential status
+    res.status(401).end();
+    return;
+  }
+
+  //console.log(userData);
+};
 
 export default handleLogin;
